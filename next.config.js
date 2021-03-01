96ccd76d2a2294b18ee77d3574eca3d8fa26d4ee
David Bau <dgbau@me.com>
2021-02-28 20:15:53 -0700

    one mroe

--- next.config.js
+++ next.config.js
@@ -1,66 +1,78 @@
-/* eslint-disable */
 const withLess = require('@zeit/next-less');
-const withSass = require('@zeit/next-sass');
 const lessToJS = require('less-vars-to-js');
+const withPlugins = require('next-compose-plugins');
+
 const fs = require('fs');
 const path = require('path');
 
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 // Where your antd-custom.less file lives
 const themeVariables = lessToJS(
-  fs.readFileSync(path.resolve(__dirname, './less/antd-custom.less'), 'utf8')
+  fs.readFileSync(path.resolve(__dirname, './less/antd-customless'), 'utf8')
 );
 
-// fix: prevents error when .less files are required by node
-if (typeof require !== 'undefined') {
-  require.extensions['.less'] = (file) => {};
-}
+const plugins = [
+  [
+    withLess({
+      lessLoaderOptions: {
+        javascriptEnabled: true,
+        modifyVars: themeVariables, // make your antd custom effective
+      },
+      webpack: (config, { isServer }) => {
+        if (isServer) {
+          const antStyles = /antd\/.*?\/style.*?/;
+          const origExternals = [...config.externals];
+          config.externals = [
+            (context, request, callback) => {
+              if (request.match(antStyles)) return callback();
+              if (typeof origExternals[0] === 'function') {
+                origExternals[0](context, request, callback);
+              } else {
+                callback();
+              }
+            },
+            ...(typeof origExternals[0] === 'function' ? [] : origExternals),
+          ];
 
-module.exports = withSass({
-  cssModules: true,
-  sassLoaderOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
-  ...withLess({
-    lessLoaderOptions: {
-      javascriptEnabled: true,
-      modifyVars: themeVariables, // make your antd custom effective
-      importLoaders: 0,
-    },
-    cssLoaderOptions: {
-      importLoaders: 3,
-      localIdentName: '[local]___[hash:base64:5]',
-    },
-    webpack: (config, { isServer }) => {
-      //Make Ant styles work with less
-      if (isServer) {
-        const antStyles = /antd\/.*?\/style.*?/;
-        const origExternals = [...config.externals];
-        config.externals = [
-          (context, request, callback) => {
-            if (request.match(antStyles)) return callback();
-            if (typeof origExternals[0] === 'function') {
-              origExternals[0](context, request, callback);
-            } else {
-              callback();
-            }
-          },
-          ...(typeof origExternals[0] === 'function' ? [] : origExternals),
-        ];
+          config.module.rules.unshift({
+            test: antStyles,
+            use: 'null-loader',
+          });
+        }
 
-        config.module.rules.unshift({
-          test: antStyles,
-          use: 'null-loader',
+        const builtInLoader = config.module.rules.find((rule) => {
+          if (rule.oneOf) {
+            return (
+              rule.oneOf.find((deepRule) => {
+                return deepRule.test && deepRule.test.toString().includes('/a^/');
+              }) !== undefined
+            );
+          }
+          return false;
         });
-        config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//));
-      }
-      return config;
-    },
-  }),
-});
 
-module.exports = {
-  /* Add Your Scss File Folder Path Here */
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
+        if (typeof builtInLoader !== 'undefined') {
+          config.module.rules.push({
+            oneOf: [
+              ...builtInLoader.oneOf.filter((rule) => {
+                return (rule.test && rule.test.toString().includes('/a^/')) !== true;
+              }),
+            ],
+          });
+        }
+
+        config.resolve.alias['@'] = path.resolve(__dirname);
+        return config;
+      },
+    }),
+  ],
+];
+
+const nextConfig = {
+  env: {},
 };
+
+module.exports = withPlugins(plugins, nextConfig);
