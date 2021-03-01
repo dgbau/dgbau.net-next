83fb18bce3c6a6e4f8b80c2e408b917bebc00b48
David Bau <dgbau@me.com>
2021-02-28 20:22:53 -0700

    ditch antd

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -1,19 +1,8 @@
 import { useState } from 'react';
 import Link from 'next/link';
-import { Menu } from 'antd';
-import {
-  MailOutlined,
-  HomeOutlined,
-  SettingOutlined,
-  DotChartOutlined,
-  MediumOutlined,
-  ExperimentOutlined,
-  GithubOutlined,
-} from '@ant-design/icons';
 
 import css from './Nav.module.scss';
 
-const { SubMenu } = Menu;
 const handleClick = (e, setCurrent) => {
   console.log('click', e.key);
   setCurrent(e.key);
@@ -21,49 +10,7 @@ const handleClick = (e, setCurrent) => {
 
 export default function Nav(props: any) {
   const [current, setCurrent] = useState('home');
-  return (
-    <Menu onClick={(e) => handleClick(e, setCurrent)} selectedKeys={[current]} mode='horizontal'>
-      {/* <h3>DG bau</h3> */}
-      <Menu.Item key='home'>
-        {' '}
-        <Link href='/'>
-          <a>DG Bau</a>
-        </Link>
-      </Menu.Item>
-      <Menu.Item key='viz' icon={<DotChartOutlined />}>
-        <Link href='/viz'>
-          <a>{null}</a>
-        </Link>
-      </Menu.Item>
-      <Menu.Item key='research' icon={<ExperimentOutlined />}>
-        <Link href='/research'>
-          <a>{null}</a>
-        </Link>
-      </Menu.Item>
-      <Menu.Item key='contact' icon={<MailOutlined />}>
-        <Link href='/contact'>
-          <a>{null}</a>
-        </Link>
-      </Menu.Item>
-      <Menu.Item key='medium' icon={<MediumOutlined />}>
-        <a href='https://medium.com' target='_blank' rel='noopener noreferrer'></a>
-      </Menu.Item>
-      <Menu.Item key='github' icon={<GithubOutlined />}>
-        <a href='https://github.com' target='_blank' rel='noopener noreferrer'></a>
-      </Menu.Item>
-      <Menu.Item key='settings' icon={<SettingOutlined />}></Menu.Item>
-      {/* <SubMenu key='settings' icon={<SettingOutlined />}>
-        <Menu.ItemGroup title='Theme'>
-          <Menu.Item key='light-theme'>Light</Menu.Item>
-          <Menu.Item key='dark-theme'>Dark</Menu.Item>
-        </Menu.ItemGroup>
-        <Menu.ItemGroup title='Language'>
-          <Menu.Item key='lang-en'>English</Menu.Item>
-          <Menu.Item key='lang-scram'>Scrambled</Menu.Item>
-        </Menu.ItemGroup>
-      </SubMenu> */}
-    </Menu>
-  );
+  return <div>Menu</div>;
 }
 
 // export const getServerSideProps = async () => {
--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,32 +1,15 @@
 // import React, { useState } from 'react';
 
 import Layout from '../../components/Layout';
-import { Form, Input, Button } from 'antd';
 const layout = {
   labelCol: { span: 24 },
   wrapperCol: { span: 24 },
 };
 
 export default function Contact() {
-  const [form] = Form.useForm();
-
   return (
     <Layout home={false}>
-      <>
-        <div className='contact-form'>
-          <Form form={form} {...layout}>
-            <Form.Item label='Name'>
-              <Input placeholder='First and Last Name' />
-            </Form.Item>
-            <Form.Item label='Email'>
-              <Input placeholder='email address' />
-            </Form.Item>
-            <Form.Item>
-              <Button type='primary'>Submit</Button>
-            </Form.Item>
-          </Form>
-        </div>
-      </>
+      <>Contact</>
     </Layout>
   );
 }
